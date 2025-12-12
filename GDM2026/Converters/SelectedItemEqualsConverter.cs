6026584fb12993c07c8218f369446c30dc2185c0
Thierrysio <55096767+Thierrysio@users.noreply.github.com>
2025-12-12 09:25:00 +0100

    Ensure event edit fields show when selected

--- GDM2026/Converters/SelectedItemEqualsConverter.cs
+++ GDM2026/Converters/SelectedItemEqualsConverter.cs
@@ -4,19 +4,27 @@ using Microsoft.Maui.Controls;
 
 namespace GDM2026.Converters;
 
-public class SelectedItemEqualsConverter : IValueConverter
+public class SelectedItemEqualsConverter : IMultiValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object[]? values, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is null || parameter is null)
+        if (values is null || values.Length < 2)
         {
             return false;
         }
 
-        return ReferenceEquals(value, parameter);
+        var selectedItem = values[0];
+        var currentItem = values[1];
+
+        if (selectedItem is null || currentItem is null)
+        {
+            return false;
+        }
+
+        return ReferenceEquals(selectedItem, currentItem) || selectedItem.Equals(currentItem);
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object[] ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
     {
         throw new NotSupportedException();
     }
